cf0b9dbec848525ff6243dce83a53466721f0f3e
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-08-16 17:47:35 +1000

    fixing responsiveness

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default function Header({title, subtext, showLine, banner}) {
     return (
-        <div className={`container mx-auto py-10 text-white ${banner ? 'center':''}`}>
+        <div className={`container mx-auto py-10 text-white ${banner ? 'hero-center':''}`}>
             <h1 className="text-3xl md:text-7xl text-center">{title}</h1>
             {subtext && <p className="text-center italic py-4">{subtext}</p>}
             {showLine && <div className="bg-title-img h-8 bg-no-repeat bg-center" style={{"backgroundSize": '496px 19px'}}></div>}
