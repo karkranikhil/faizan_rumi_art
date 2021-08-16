b9e9448539663a2632b35418318ebff0ed13aef5
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-08-16 17:20:41 +1000

    removing purgin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
